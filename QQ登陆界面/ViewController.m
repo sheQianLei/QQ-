03e54d91c1252371a3cce6a8d65e3bed34e570ca
news <690820279@qq.com>
2016-07-23 20:54:27 +0800

    ni

--- QQ登陆界面/ViewController.m
+++ QQ登陆界面/ViewController.m
@@ -9,19 +9,34 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *QQ;
+@property (weak, nonatomic) IBOutlet UITextField *passWord;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)getUp:(id)sender {
+    
+    if ([_QQ.text isEqualToString:@"123"]&&[_passWord.text isEqualToString:@"abc"]) {
+    
+        CGFloat x = self.view.frame.size.width / 2 - 100;
+        CGFloat y = self.view.frame.size.height / 2;
+        CGFloat w = 100;
+        CGFloat h = 50;
+        UILabel *lab = [[UILabel alloc] initWithFrame:CGRectMake(x, y, w, h)];
+        lab.text = @"登陆成功";
+    
+        lab.center = self.view.center;
+    
+        [self.view addSubview:lab];
+        
+    
+        
+        
+        
+    }
+    
 }
 
 @end
